75159f8230d8ffc65f51e1860ab4b4184980786d
Nattapol Chanpaisit <66078943+NattapolChan@users.noreply.github.com>
2022-03-11 21:59:09 +0700

    Add login page(just interface)

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,76 +1,69 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+import { GoogleLoginButton} from 'ts-react-google-login-component';
 
-const Login: NextPage = () => {
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+class Login extends React.Component {
 
-            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-                <h1 className="text-6xl font-bold">
-                    Welcome to{' '}
-                    <a className="text-blue-600" href="https://nextjs.org">
-                        Next.js!
-                    </a>
-                </h1>
+    preLoginTracking(): void {
+        console.log('Attemp to login with google');
+    }
 
-                <p className="mt-3 text-2xl">
-                    Get started by editing{' '}
-                    <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-                        pages/index.tsx
-                    </code>
-                </p>
+    errorHandler(error: string): void{
+        console.error(error)
+    }
 
-                <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-                    <a
-                        href="https://nextjs.org/docs"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Find in-depth information about Next.js features and API.
-                        </p>
-                    </a>
+    responseGoogle(googleUser: gapi.auth2.GoogleUser): void {
+        const id_token = googleUser.getAuthResponse(true).id_token
+        const googleId = googleUser.getId()
 
-                    <a
-                        href="https://nextjs.org/learn"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Learn about Next.js in an interactive course with quizzes!
-                        </p>
-                    </a>
+        console.log({ googleId })
+        console.log({accessToken: id_token})
 
-                    <a
-                        href="https://github.com/vercel/next.js/tree/canary/examples"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Discover and deploy boilerplate example Next.js projects.
-                        </p>
-                    </a>
+    }
 
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
+    render(): JSX.Element {
+        const clientConfig = { client_id: 'youappid' }
+        const signInOptions = { scope: 'profile' }
+        return (
+        <div>
+            <div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+                <div className="w-full sm:max-w-md p-5 mx-auto">
+                    <h2 className="mb-12 text-center text-5xl font-extrabold">Login</h2>
+                    <div className="divide-y-4 divide-slate-700 divide-dashed outline p-4 outline-offset-4">
+                        <div>
+                            <div className="mb-4">
+                                <label className="block mb-1">Email-Address</label>
+                                <input id="email" type="text" name="email" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block mb-1">Password</label>
+                                <input id="password" type="password" name="password" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+                            </div>
+                            <div className="mb-2 grid justify-items-center">
+                                <button className="rounded bg-green-700 hover:bg-green-900 
+                                py-2 px-4 text-white"> Sign up</button>
+                            </div>
+                            <div className="mb-6 text-center">
+                                <a href="#" className="text-sm"> Forgot your password? </a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mt-6 flex items-center justify-center">
+                                <GoogleLoginButton
+                                    responseHandler={this.responseGoogle}
+                                    clientConfig={clientConfig}
+                                    preLogin={this.preLoginTracking}
+                                    failureHandler={this.errorHandler}
+                                    singInOptions={signInOptions}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </main>
-
-
+            </div>
         </div>
-    )
+        )
+    }
+
 }
 
-export default Login
+export default Login;
